8f228357798d4fba7c5b7de26100b0f678741b3b
Hüseyin SEVİM <hsyn_svm@hotmail.com>
2024-09-19 22:54:40 +0300

    YöneticiGirişiTasarlandı

--- YargimBlogWebApp/YargimBlogWebApp/YoneticiPanel/Giris.aspx
+++ YargimBlogWebApp/YargimBlogWebApp/YoneticiPanel/Giris.aspx
@@ -4,13 +4,46 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title>YargimBlogWebApp</title>
+    <title>YargimBlogWebAppYoneticiPanelGiris</title>
     <link href="Css/girisCss.css" rel="stylesheet" />
 </head>
 <body>
     <form id="form1" runat="server">
         <div class="tasiyici">
-            
+            <div class="sol">
+                <h3>Giriş Yapınız</h3>
+                <div style="border-bottom:1px solid silver;padding-bottom:10px;margin:10px 0;"></div>
+                <asp:Panel ID="pnl_basarisiz" runat="server" CssClass="panelBasarisiz" Visible="false">
+                    <asp:Label ID="lbl_mesaj" runat="server">Kullanıcı Bulunamadı</asp:Label>   
+                </asp:Panel>
+                <div class="satir">
+                    <label>Mail</label><br />
+                    <asp:TextBox ID="tb_mail" runat="server" CssClass="metinKutu" placeholder="@hotmail.com" Text="hsynsvm@hotmail.com"></asp:TextBox>
+                </div>
+                <div class="satir">
+                    <label>Şifre</label><br />
+                    <asp:TextBox ID="tb_sifre" runat="server" CssClass="metinKutu" placeholder="123?***" TextMode="Password"></asp:TextBox>
+                </div>
+                <div class="satir">
+                    <asp:Button ID="btn_girisYap" runat="server" OnClick="btn_girisYap_Click" Text="Yönetici Giriş" CssClass="girisButton" />
+                </div>
+                <div class="satir">
+                    <a href="#" class="sifre_unuttum">Şifremi Unuttum :/</a>
+                </div>
+            </div>
+            <div class="sag">
+                <h2 class="baslik">Yönetici Giriş Paneline Hoşgeldiniz</h2>
+                <br />
+                <br />
+                Bu Alandan Üye Girişi Yapılmamaktadır!
+                <br />
+                Üye Girişi Yapmak İçin
+                <br />
+                <br />
+                <br />
+                <a href="#" class="uyelink">Üye Giriş</a>
+            </div>
+            <div style="clear:both"></div>
         </div>
     </form>
 </body>
--- YargimBlogWebApp/YargimBlogWebApp/YoneticiPanel/Giris.aspx.cs
+++ YargimBlogWebApp/YargimBlogWebApp/YoneticiPanel/Giris.aspx.cs
@@ -4,14 +4,63 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VeriErisimKatmani;
 
 namespace YargimBlogWebApp.YoneticiPanel
 {
     public partial class Giris : System.Web.UI.Page
     {
+        VeriModel veriModel = new VeriModel();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btn_girisYap_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tb_mail.Text))
+            {
+                if (!string.IsNullOrEmpty(tb_sifre.Text))
+                {
+                    Yonetici yonetici = veriModel.YoneticiGiris(tb_mail.Text,tb_sifre.Text);
+                    if (yonetici != null)
+                    {
+                        if (yonetici.ID != 0)
+                        {
+                            if (yonetici.Durum)
+                            {
+                                Session["GirisYapanYonetici"] = yonetici;
+                                Response.Redirect("YoneticiDefault.aspx");
+                            }
+                            else
+                            {
+                                pnl_basarisiz.Visible = true;
+                                lbl_mesaj.Text = "Hesabınız Askıya Alınmıştır";
+                            }
+                        }
+                        else
+                        {
+                            pnl_basarisiz.Visible = true;
+                            lbl_mesaj.Text = "Kullanıcı Bulunamadı!";
+                        }
+                    }
+                    else
+                    {
+                        pnl_basarisiz.Visible=true;
+                        lbl_mesaj.Text = "Bir Hata Oluştu";
+                    }
+                }
+                else
+                {
+                    pnl_basarisiz.Visible=true;
+                    lbl_mesaj.Text = "Şifre alanı boş bırakılamaz!";
+                }
+            }
+            else
+            {
+                pnl_basarisiz.Visible = true;
+                lbl_mesaj.Text = "Mail adresi boş bırakılamaz!";
+            }
         }
     }
 }
